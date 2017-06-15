369a1d7c4553cd67e0a43de24b61b62ad5be6c94
diamond-shine <focusingdev@outlook.com>
2017-06-14 23:57:37 -0700

    Add scrollback functionality using websockets

--- chatdemo/consumers.py
+++ chatdemo/consumers.py
@@ -73,3 +73,37 @@ def broadcast_presence(sender, room, **kwargs):
             }
         })
     })
+
+@channel_session_user_from_http
+def loadhistory_connect(message):
+    message.reply_channel.send({"accept": True})
+
+@channel_session_user
+def loadhistory_receive(message):
+    data = json.loads(message['text'])
+    chat_queryset = ChatMessage.objects.filter(id__lte=data['last_message_id']).order_by("-created")[:10]
+    chat_message_count = len(chat_queryset)
+    if chat_message_count > 0:
+        first_message_id = chat_queryset[len(chat_queryset)-1].id
+    else:
+        first_message_id = -1
+    previous_id = -1
+    if first_message_id != -1:
+        try:
+            previous_id = ChatMessage.objects.filter(pk__lt=first_message_id).order_by("-pk")[:1][0].id
+        except IndexError:
+            previous_id = -1
+
+    chat_messages = reversed(chat_queryset)
+    cleaned_chat_messages = list()
+    for item in chat_messages:
+        current_message = item.message_html
+        cleaned_item = {'user' : item.user.username, 'message' : current_message }
+        cleaned_chat_messages.append(cleaned_item)
+
+    my_dict = { 'messages' : cleaned_chat_messages, 'previous_id' : previous_id }
+    message.reply_channel.send({'text': json.dumps(my_dict)})
+
+@channel_session_user
+def loadhistory_disconnect(message):
+    pass
--- chatdemo/routing.py
+++ chatdemo/routing.py
@@ -1,6 +1,6 @@
 from channels.routing import route
 from channels import include
-from chatdemo.consumers import chat_connect, chat_disconnect, chat_receive
+from chatdemo.consumers import chat_connect, chat_disconnect, chat_receive, loadhistory_connect, loadhistory_disconnect, loadhistory_receive
 
 chat_routing = [
     route("websocket.connect", chat_connect),
@@ -8,6 +8,13 @@ chat_routing = [
     route("websocket.disconnect", chat_disconnect)
 ]
 
+loadhistory_routing = [
+    route("websocket.connect", loadhistory_connect),
+    route("websocket.receive", loadhistory_receive),
+    route("websocket.disconnect", loadhistory_disconnect)
+]
+
 channel_routing = [
     include(chat_routing, path=r"^/ws/$"),
+    include(loadhistory_routing, path=r"^/loadhistory/$"),
 ]
